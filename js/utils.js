4f81f737807a669c8c0b10b6e3b4a86c551ea9c6
m.kikmunter <M.Kikmunter@gmail.com>
2019-10-20 22:08:11 +0100

    moved loader code to utils - later to own file

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,31 @@
 const utils = function () {
 
+    const loaderVisible = "loader-visible";
+    const loaderHidden = "loader-hidden";
+
     const setFooterDate = () => {
         document.getElementById("copyright").insertAdjacentText("beforeend", ` ${new Date().getFullYear()}`);
     };
 
+    const getLoader = () => {
+        return document.getElementsByClassName("loader")[0];
+    }
+
+    const showLoader = () => {
+        const loader = getLoader();
+        loader.classList.remove(loaderHidden);
+        loader.classList.add(loaderVisible);
+    }
+
+    const hideLoader = () => {
+        const loader = getLoader();
+        loader.classList.remove(loaderVisible);
+        loader.classList.add(loaderHidden);
+    };
+
     return {
-        setFooterDate: setFooterDate
+        setFooterDate: setFooterDate,
+        showLoader: showLoader,
+        hideLoader: hideLoader
     }
 }();
